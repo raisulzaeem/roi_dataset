ecd0fef701303ba4b7482da2207ebb85153ea077
Raisul <raisulzaeem@gmail.com>
2023-02-17 09:30:46 +0100

    ensure first we can resize and write the image, then we create the gaussian image. This addresses the issue that, when we fail to work with an image, the program will not crash

--- create_gaussian_dataset.py
+++ create_gaussian_dataset.py
@@ -79,10 +79,9 @@ if __name__ == "__main__":
             image_height, image_width, _ = image.shape
             roi_pixel = [roi*mm_to_pixel for roi in roi_mm]
             roi_in_percent = [roi_pixel[0]/image_width, roi_pixel[1]/image_height, roi_pixel[2]/image_width, roi_pixel[3]/image_height]
-            create_gaussian_image(local_image_path,roi_in_percent, gaussian_dir, dimension=2048)
-
             if not resize_image(local_image_path, image_dir_2048):
                 continue
+            create_gaussian_image(local_image_path,roi_in_percent, gaussian_dir, dimension=2048)
 
             local_images_roi_percentage_dict.update({local_image_path:roi_in_percent})
         except Exception as e:
@@ -90,9 +89,6 @@ if __name__ == "__main__":
 
         with open("images_roi_percent_latest.json",'w') as f:
             json.dump(local_images_roi_percentage_dict,f)
-
-    
-
 
 
 
